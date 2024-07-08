668555a3feb60345cb8a046ecc1d422d7d73cf4d
RedHotChilliHead <irina.purgina@inbox.ru>
2024-07-08 14:46:37 +0530

    init a project with an app

---
+++ ForFar/forfarapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForfarappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forfarapp'
---
+++ ForFar/forfarapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = "forfarapp"
+
+urlpatterns = [
+    # path('', .as_view(), name=''),
+]
